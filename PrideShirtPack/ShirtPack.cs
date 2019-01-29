11b8c717ac8955e88d24651c00b60dd0e5226dca
Joanie Davis <joanie.davis@atomicobject.com>
2019-01-28 20:57:01 -0500

    The mod works! It takes a while to load, but it works!

---
+++ PrideShirtPack/ShirtPack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using StardewModdingAPI;
+
+namespace PrideShirtPack
+{
+    public class ShirtPack: Mod, IAssetLoader
+    {
+
+        public override void Entry(IModHelper helper)
+        {
+            this.Monitor.Log($"loading pride mod.");
+        }
+
+        /// <summary>Get whether this instance can load the initial version of the given asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public bool CanLoad<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Characters/Farmer/shirts"))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>Load a matched asset.</summary>
+        /// <param name="asset">Basic metadata about the asset being loaded.</param>
+        public T Load<T>(IAssetInfo asset)
+        {
+            if (asset.AssetNameEquals("Characters/Farmer/shirts"))
+            {
+                return this.Helper.Content.Load<T>("assets/shirts_with_pride.png", ContentSource.ModFolder);
+            }
+
+            throw new InvalidOperationException($"Unexpected asset '{asset.AssetName}'.");
+        }
+    }
+}
